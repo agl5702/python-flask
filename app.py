c2bfbd32294a3b4121020f514b79c90139dca4f9
agl5702 <advasquez41@misena.edu.co>
2023-09-01 10:51:30 -0500

    second commit angel

--- app.py
+++ app.py
@@ -1,29 +1,34 @@
 #Importamos flask
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 import numpy as np
+
 app= Flask(__name__)
+CORS(app)
 
 #definimos la ruta principal
 @app.route("/")
 def HolaFlask():
     return "<h1>¡Hola Flask!</h1> <hr>"
 
-#definimos una segunda ruta
+
+
+
+
+
+
+
 @app.route("/notas")
-#definimos una tercera ruta
+
 @app.route("/notas/<float:nota1>/<float:nota2>/<float:nota3>")
-@app.route("/edades")
-@app.route("/edades/<int:edad>")
-@app.route("/arreglos")
-@app.route("/arreglos/<int:valores>/<int:columnas>")
-@app.route("/arreglos/<int:valores>/<int:columnas>/<int:filas>")
-
-
-
 def notas(nota1=0,nota2=0,nota3=0):
     resultado=(nota1*30)/100+(nota2*30)/100+(nota3*40)/100
-    return f"<h1> El resultado es: {resultado}</h1> <hr>"
-    
+    return jsonify({"resultado": resultado})
+
+
+
+@app.route("/edades")
+@app.route("/edades/<int:edad>")
 def edades(edad=0):
     if edad <18:
         r= "Menor de edad"
@@ -31,17 +36,24 @@ def edades(edad=0):
         r="Adulto"
     else:
         r="Adulto mayor"
-    return f"<h1> La persona es : {r} </h1> <hr>"
+    return jsonify({"resultado": r})
 
+
+
+@app.route("/arreglos")
+@app.route("/arreglos/<int:valores>/<int:columnas>")
+@app.route("/arreglos/<int:valores>/<int:columnas>/<int:filas>")
 def arreglos(valores=0, columnas=0, filas=0):
     if filas == 0:
         arreglo = np.random.randint(valores, size=columnas)
     else:
         arreglo = np.random.randint(valores, size=(filas, columnas))
         
-    return f"<h1>El arreglo aleatorio es: {arreglo}</h1> <hr>"
+    return jsonify({"arreglo": arreglo.tolist()})
 
 
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(debug=True) 
+    
+    
